1175c8abf6145f7569f94dcfb5dfed464d8b272a
Chuang Wang <chuangw.sde@gmail.com>
2020-11-22 12:53:58 +1100

    remove shell script

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/Users/frank/opt/anaconda3/bin/python src/download_data.py --url=https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data --out_file_path=data/raw/abalone.csv
