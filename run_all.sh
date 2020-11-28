1b617d0ed510d86c8c5e8f6764872a75ec0599f3
Chuang Wang <chuangw.sde@gmail.com>
2020-11-28 11:31:06 +1100

    update shell script

--- run_all.sh
+++ run_all.sh
@@ -5,7 +5,14 @@
 # 
 # Usage: bash run_all.sh
 
+# download the raw data froom UCI website
+python src/data_wrangling/download_data.py --url=https://archive.ics.uci.edu/ml/machine-learning-databases/abalone/abalone.data --out_file_path=data/raw/abalone.csv
 
+# preprocess the data and split it into train and test set 
+python src/data_wrangling/pre_process_abalone_data.py --input=data/raw/abalone.csv --out_dir=data/preprocess
+
+# run EDA process
+python src/eda/abalone_eda.py --train=data/preprocess/training.csv --out_dir=results/eda
 
 # train ML model and plot hyperparameter tuning result
 python src/ml/abalone_fit_predict_model.py --train=data/raw/abalone.csv --out_dir=results/ml_model
